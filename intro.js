ae659bf07f1dcc5d6ac75c1465589ab6618f89c2
Mitrea Andrei Razvan <razvanmitrea1@gmail.com>
2019-03-11 21:00:00 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,23 +1,6 @@
-console.info("my first js file");
-
-var name="Mitrea";
-var employed=true;
-
-console.warn("employed");
-console.warn(employed);
-
-
-console.info("name:");
-console.info(name);
-
-console.info("name:");
-console.info(name);
-
-
 function getWellcomeMsg() {
     return "Welcome to my site!";
 }
-
 
 function getColor() {
     var date = new Date();
@@ -36,58 +19,14 @@ console.info("socialElement", socialElement);
 var color = getColor();
 socialElement.style.color = color;
 
-//first example
-function hideHomePage() {
-    document.getElementById('home-page').style.display = 'none';
-}
 
-function showSkillsPage() {
-    document.getElementById('skills-page').style.display = 'block';
-}
-function clickOnSkills(){
-    //hideHomePage();
-    //showSkillsPage();
-    hideAllpages()
-    hidePage('home-page');
-    hidePage('languages-page');
-    hidePage('projects-page');
-    showPage('skills-page');
-}
-
-function clickOnHome(){
-    hideAllpages()
-    hidePage('skills-page');
-    hidePage('languages-page');
-    hidePage('projects-page');
-    showPage('home-page');
-}
-function clickOnProjects(){
-
-    hideAllpages()
-    hidePage('skills-page');
-    hidePage('languages-page');
-    hidePage('home-page');
-    showPage('projects-page');
-}
-function clickOnLanguages(){
-    hideAllpages()
-    hidePage('skills-page');
-    hidePage('home-page');
-    hidePage('projects-page');
-    showPage('languages-page');
-}
 
 
 function hidePage(page) {
-    document.getElementById(page).style.display = 'none';
+   var el = document.getElementById(page).style.display = 'none';
+   el.style.display = 'none';
 }
 
 function showPage(page) {
     document.getElementById(page).style.display = 'block';
-}
-
-function hideAllpages(){
-var pages = document.getElementsByClassName('page');
-for (var i = 0; i< pages.length; i++)
-pages[i].style.display = 'none';
 }
